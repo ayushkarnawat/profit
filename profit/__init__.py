e21a7c47d944a6c41bfb363d20324bce30f84685
ayushkarnawat <ayush.karnawat97@gmail.com>
2019-11-11 20:40:32 -0500

    Add amino acid residue geometries

--- profit/__init__.py
+++ profit/__init__.py
@@ -5,4 +5,5 @@ from profit import cyclops
 from profit import dataset
 from profit import layers
 from profit import models
+from profit import peptide_builder
 from profit import utils
---
+++ profit/peptide_builder/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module for creating peptide models in PDB format based on geometrical parameters. 
+Adapted from https://github.com/mtien/PeptideBuilder.
+"""
+
+from profit.peptide_builder import geometry
