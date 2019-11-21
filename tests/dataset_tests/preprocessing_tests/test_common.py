d61308860638604fc5e6f4ebb0784b904e62a6e1
ayushkarnawat <ayush.karnawat97@gmail.com>
2019-11-20 20:20:45 -0500

    Update molecule feature extraction tests

--- tests/dataset_tests/preprocessing_tests/test_common.py
+++ tests/dataset_tests/preprocessing_tests/test_common.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 
 from rdkit.Chem import rdmolops, rdmolfiles, rdDistGeom
-from profit.dataset.preprocessors import common
+from profit.dataset.preprocessing import mol_feats
 
 
 @pytest.fixture
@@ -24,7 +24,7 @@ def sample_mol2():
 class TestGetAdjMatrix(object):
 
     def test_normal(self, sample_mol2):
-        adj = common.construct_adj_matrix(sample_mol2, normalize=False)
+        adj = mol_feats.construct_adj_matrix(sample_mol2, normalize=False)
 
         assert adj.shape == (7, 7)
         expect = np.array([[1., 1., 0., 0., 0., 0., 0.],
@@ -37,7 +37,7 @@ class TestGetAdjMatrix(object):
         np.testing.assert_equal(adj, expect)
 
     def test_normal_no_self_connection(self, sample_mol2):
-        adj = common.construct_adj_matrix(sample_mol2, add_self_loops=False, normalize=False)
+        adj = mol_feats.construct_adj_matrix(sample_mol2, add_self_loops=False, normalize=False)
 
         assert adj.shape == (7, 7)
         expect = np.array([[0., 1., 0., 0., 0., 0., 0.],
@@ -50,7 +50,7 @@ class TestGetAdjMatrix(object):
         np.testing.assert_equal(adj, expect)
 
     def test_normal_padding(self, sample_mol2):
-        adj = common.construct_adj_matrix(sample_mol2, out_size=8, add_self_loops=True, normalize=False)
+        adj = mol_feats.construct_adj_matrix(sample_mol2, out_size=8, add_self_loops=True, normalize=False)
 
         assert adj.shape == (8, 8)
         expect = np.array([[1., 1., 0., 0., 0., 0., 0., 0.],
@@ -65,4 +65,4 @@ class TestGetAdjMatrix(object):
 
     def test_normal_truncated(self, sample_mol2):
         with pytest.raises(ValueError):
-            _ = common.construct_adj_matrix(sample_mol2, out_size=4)
+            _ = mol_feats.construct_adj_matrix(sample_mol2, out_size=4)
