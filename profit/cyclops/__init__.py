f3659a7f203037c4e30cf63f9251f561e3b51c94
ayushkarnawat <ayush.karnawat97@gmail.com>
2019-11-24 16:43:20 -0500

    Remove invalid preprocessing (aka conversion from string to SMILES to 3D coords)

--- profit/cyclops/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Module to convert peptide sequences into smiles and 2D and 3D molecular file formats.
-Modified from https://github.com/fergaljd/cyclops.
-"""
-from profit.cyclops import aminoacids
-from profit.cyclops import struct_gen
--- profit/dataset/__init__.py
+++ profit/dataset/__init__.py
@@ -2,5 +2,4 @@ from profit.dataset import parsers
 from profit.dataset import preprocessing
 from profit.dataset import preprocessors
 from profit.dataset import splitters
-from profit.dataset import converter
 from profit.dataset import generator
