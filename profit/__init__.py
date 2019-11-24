4199604764631ad70d86bf4bb8721040a74c3aa3
ayushkarnawat <ayush.karnawat97@gmail.com>
2019-11-23 20:24:54 -0500

    Cleanup snippets in remaining files

--- profit/__init__.py
+++ profit/__init__.py
@@ -1,6 +1,3 @@
-# from profit.cyclops import aminoacids, struct_gen
-# from profit.dataset.splitter import base_splitter, random_splitter
-
 from profit import cyclops
 from profit import dataset
 from profit import layers
