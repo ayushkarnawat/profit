74a5496b933bd00b16481d9afe4422168c258fcc
ayushkarnawat <ayush.karnawat97@gmail.com>
2020-02-11 00:08:37 -0500

    Move tf/keras training utils to own folder

--- profit/utils/training_utils/__init__.py
+++ profit/utils/training_utils/__init__.py
@@ -1,7 +1,1 @@
-from profit.utils.training_utils import callbacks
-from profit.utils.training_utils import metrics
-
-from profit.utils.training_utils.callbacks import XTensorBoard
-from profit.utils.training_utils.metrics import std_mae
-from profit.utils.training_utils.metrics import std_rmse
-from profit.utils.training_utils.metrics import std_r2
+from profit.utils.training_utils import tensorflow
---
+++ profit/utils/training_utils/tensorflow/__init__.py
@@ -0,0 +1,7 @@
+from profit.utils.training_utils.tensorflow import callbacks
+from profit.utils.training_utils.tensorflow import metrics
+
+from profit.utils.training_utils.tensorflow.callbacks import XTensorBoard
+from profit.utils.training_utils.tensorflow.metrics import std_mae
+from profit.utils.training_utils.tensorflow.metrics import std_rmse
+from profit.utils.training_utils.tensorflow.metrics import std_r2
