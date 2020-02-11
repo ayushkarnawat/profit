03c42b1d169bd539ad9f39d81c1c422d13621e8b
ayushkarnawat <ayush.karnawat97@gmail.com>
2020-02-11 13:01:38 -0500

    Add performance metrics for pytorch training tasks

--- profit/utils/training_utils/__init__.py
+++ profit/utils/training_utils/__init__.py
@@ -1,1 +1,2 @@
+from profit.utils.training_utils import pytorch
 from profit.utils.training_utils import tensorflow
---
+++ profit/utils/training_utils/pytorch/__init__.py
@@ -0,0 +1,7 @@
+from profit.utils.training_utils.pytorch import metrics
+
+from profit.utils.training_utils.pytorch.metrics import accuracy
+from profit.utils.training_utils.pytorch.metrics import mae
+from profit.utils.training_utils.pytorch.metrics import mse
+from profit.utils.training_utils.pytorch.metrics import rmse
+from profit.utils.training_utils.pytorch.metrics import spearmanr
