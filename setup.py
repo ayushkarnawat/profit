69c618f0700a6871d2709e3ee04a7f126eba9255
ayushkarnawat <ayush.karnawat97@gmail.com>
2020-03-12 23:25:15 -0400

    Minor changes

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-import os
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='profit',
-      version='0.1dev',
-      description='Protein Fitness: Determine evolutionary protein fitness',
-      author = 'Ayush Karnawat',
-      author_email='ayushkarnawat@gmail.com',
+setup(name="profit",
+      version="0.1dev",
+      description="Profit: Determine evolutionary protein fitness",
+      long_description=open("README.md", encoding="utf-8").read(),
+      author="Ayush Karnawat",
+      author_email="ayushkarnawat@gmail.com",
+      url="https://github.com/ayushkarnawat/profit",
       packages=find_packages(),
-      license='MIT',
-      url='https://github.com/ayushkarnawat/profit',
-      long_description=open('README.md', encoding='utf-8').read(),
-      long_description_content_type='text/markdown'
-    )
+      license="MIT",
+      long_description_content_type="text/markdown")
