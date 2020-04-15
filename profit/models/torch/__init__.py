4b5eecd3fdc1d10a2d14e1125734fc34ceea32f6
Ayush Karnawat <ayush.karnawat97@gmail.com>
2020-04-15 15:35:25 -0400

    Convert numpy gpr to torch gpr (#109)
    * Add torch gpr
    
    * Allow smatrix to be torch.Tensor
    
    * Train using torch gpr
    
    * Remove numpy version

--- profit/models/torch/__init__.py
+++ profit/models/torch/__init__.py
@@ -1,9 +1,11 @@
 """PyTorch models."""
 
 from profit.models.torch import egcn
+from profit.models.torch import gpr
 from profit.models.torch import lstm
 from profit.models.torch import vae
 
 from profit.models.torch.egcn import EmbeddedGCN
+from profit.models.torch.gpr import SequenceGPR
 from profit.models.torch.lstm import LSTMModel
 from profit.models.torch.vae import SequenceVAE
