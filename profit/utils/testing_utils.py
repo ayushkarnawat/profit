a899e448f95c38cc43efb38426b5f20beaa859a9
ayushkarnawat <ayush.karnawat97@gmail.com>
2020-04-21 17:53:03 -0400

    Docs

--- profit/utils/testing_utils.py
+++ profit/utils/testing_utils.py
@@ -1,17 +1,13 @@
-"""Testing utils.
+"""Testing utils."""
 
-TODO: Have more general prediction util where the user provides the mu
-and var for each of the oracles as input rather than just passing in the
-oracles.
-"""
-
-from typing import List
+import typing
 
 import torch
 from torch.nn import functional as F
 
 
-def avg_oracle_preds(oracles: List[torch.nn.Module], Xt: torch.Tensor):
+def avg_oracle_preds(oracles: typing.List[torch.nn.Module], X: torch.Tensor
+                     ) -> typing.Tuple[torch.Tensor, torch.Tensor]:
     """Average oracle predictions equally.
 
     NOTE: All oracles/models MUST have the same input and output shapes.
@@ -20,13 +16,13 @@ def avg_oracle_preds(oracles: List[torch.nn.Module], Xt: torch.Tensor):
         oracles = [oracles]
 
     num_oracles = len(oracles)
-    num_samples = Xt.size(0)
+    num_samples = X.size(0)
     means = torch.zeros(num_oracles, num_samples)
     var = torch.zeros(num_oracles, num_samples)
     for idx, oracle in enumerate(oracles):
         oracle.eval()
         with torch.no_grad():
-            pred = oracle(Xt)
+            pred = oracle(X)
             means[idx, :] = pred[:, 0]
             var[idx, :] = F.softplus(pred[:, 1]) + 1e-6 # positivity constraint
     # Average preds across all oracles
