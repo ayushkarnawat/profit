a6cfed502dbb8c7719c4d0690d22946920596663
ayushkarnawat <ayush.karnawat97@gmail.com>
2020-05-11 16:46:51 -0400

    Semantic versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name="profit",
-      version="0.1dev",
+      version="0.1.0",
       description="Profit: Determine evolutionary protein fitness",
       long_description=open("README.md", encoding="utf-8").read(),
       author="Ayush Karnawat",
